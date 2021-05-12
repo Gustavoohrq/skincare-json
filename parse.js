36dcf3045643266f24f0c1bf6be7630422e8d669
Gustavo Henrique <gustavofmariano@gmail.com>
2021-05-12 12:09:52 -0300

    refactor: add var in function

--- parse.js
+++ parse.js
@@ -13,7 +13,7 @@ module.exports = function parse(jsonDirty) {
           .replace(/([a-zA-Z])("")(,)/g, '$1"$3')
           .replace(/[']/g, '')
           .replace(/[\n\r]/g, "")
-  var jsonFormatedAndClean = JSON.parse
+  var jsonFormatedAndClean = JSON.parse(jsonDirty)
   return jsonFormatedAndClean        
 }
         
