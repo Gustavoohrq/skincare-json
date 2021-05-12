bdbdc74d8e29a11306c89ac26bc4707cd4eed9c7
Gustavo Henrique <gustavofmariano@gmail.com>
2021-05-12 12:47:12 -0300

    feat: new replaces quotes

--- parse.js
+++ parse.js
@@ -1,19 +1,21 @@
 module.exports = function parse(jsonDirty) {
   jsonDirty = JSON.stringify(jsonDirty)
-          .replace(/(\".*\":)\"\"(.*)\"\"/g, '$1 ""$2\\""').replace(/\\"/g, '"')
-          .replace(/\\"/g, '"')
-          .replace(/:",/g, ': "",')
-          .replace(/([0-9]+),([0-9]+)/g, '$1$2')
-          .replace(/^\"|\"$/g, "")
-          .replace(/: ",/g, ': "",')
-          .replace(/" ",/g, '",')
-          .replace(/([a-zA-Z])\s("+)\s([a-zA-Z])/g, '$1 $3')
-          .replace(/([a-zA-Z])("+)\s([a-zA-Z])/g, '$1 $3')
-          .replace(/([a-zA-Z])\s("+)([a-zA-Z])/g, '$1 $3')
-          .replace(/([a-zA-Z])("")(,)/g, '$1"$3')
-          .replace(/[']/g, '')
-          .replace(/[\n\r]/g, "")
+    .replace(/(\".*\":)\"\"(.*)\"\"/g, '$1 ""$2\\""').replace(/\\"/g, '"')
+    .replace(/\\"/g, '"')
+    .replace(/:",/g, ': "",')
+    .replace(/([0-9]+),([0-9]+)/g, '$1$2')
+    .replace(/^\"|\"$/g, "")
+    .replace(/: ",/g, ': "",')
+    .replace(/" ",/g, '",')
+    .replace(/([a-zA-Z])\s("+)\s([a-zA-Z])/g, '$1 $3')
+    .replace(/([a-zA-Z])("+)\s([a-zA-Z])/g, '$1 $3')
+    .replace(/([a-zA-Z])\s("+)([a-zA-Z])/g, '$1 $3')
+    .replace(/([a-zA-Z])("")(,)/g, '$1"$3')
+    .replace(/(:)("")\s([a-zA-Z])/g, '$1 "$3')
+    .replace(/(:)\s("")\s([a-zA-Z])/g, '$1 "$3')
+    .replace(/[']/g, '')
+    .replace(/[\n\r]/g, "")
   var jsonFormatedAndClean = JSON.parse(jsonDirty)
-  return jsonFormatedAndClean        
+  return jsonFormatedAndClean
 }
-        
+
